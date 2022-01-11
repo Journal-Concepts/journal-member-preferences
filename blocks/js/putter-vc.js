ede2879c16250823471c6fa6f9094f0e259f53b7
Roger Coathup <roger@21applications.com>
2022-01-11 13:16:48 +0000

    Additional JS protect

--- blocks/js/putter-vc.js
+++ blocks/js/putter-vc.js
@@ -1,11 +1,12 @@
-( function() { 
+{
+	( function() { 
 	const { registerBlockType } = wp.blocks;
 	const { createElement } = wp.element;
 	const { __ } = wp.i18n;
 
 	registerBlockType( 'journal-member-preferences/putter-vc', {
 		title: __( 'Putter VC' ), // Block title.
-		category:  __( 'circ-management' ), //category
+		category:  __( 'common' ), //category
 		attributes:  {},
 		edit: props => {
 			const { attributes, setAttributes } = props;
@@ -18,3 +19,4 @@
 		}
 	})
 })();
+}
