61024f73728333076805d7f5738c5be91818a400
Roger Coathup <roger@21applications.com>
2022-09-13 09:39:22 +0100

    Adding a some division / clarity to the page

--- blocks/headcover-vc.php
+++ blocks/headcover-vc.php
@@ -114,7 +114,8 @@ class JC_Member_Preferences_Headcover_VC {
 		
 			<div class="control">
 		
-				<a class="button" id="generate-headcover-report" href="#">Generate Report</a>
+				<a class="button" id="generate-headcover-report" href="#">Generate Status Report</a>
+				<hr/>
 
 				<form id="redeem-headcovers" name="redeem-headcovers" method="post">
 					<?php wp_nonce_field( 'jc-preference-redeem-headcovers' );?>
@@ -153,6 +154,7 @@ class JC_Member_Preferences_Headcover_VC {
 				</form>
 				
 			</div>
+			<hr/>
 		
 			<h3>Status reports</h3>
 			<div class="reports">
